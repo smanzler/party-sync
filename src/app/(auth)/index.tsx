3214b34e40ad89be8b2bb89f79e8acbb81f8e07f
Simon Manzler <simanzler@gmail.com>
2025-10-23 20:35:06 -0400

    updated auth index styling

--- src/app/(auth)/index.tsx
+++ src/app/(auth)/index.tsx
@@ -1,28 +1,42 @@
 import Button from "@/src/components/ui/button";
+import Text from "@/src/components/ui/text";
+import { useTheme } from "@/src/providers/ThemeProvider";
 import { router } from "expo-router";
+import { Gamepad2 } from "lucide-react-native";
 import { View } from "react-native";
 
 export default function Index() {
+  const { colors } = useTheme();
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        gap: 8,
+        gap: 100,
         padding: 16,
       }}
     >
-      <Button onPress={() => router.push("/login")} fullWidth>
-        Login
-      </Button>
-      <Button
-        onPress={() => router.push("/sign-up")}
-        variant="secondary"
-        fullWidth
-      >
-        Sign Up
-      </Button>
+      <View style={{ gap: 8, alignItems: "center" }}>
+        <Gamepad2 size={150} color={colors.text} />
+        <Text size="5xl" bold>
+          PartySync
+        </Text>
+      </View>
+
+      <View style={{ gap: 8, width: "100%" }}>
+        <Button onPress={() => router.push("/login")} fullWidth>
+          Login
+        </Button>
+        <Button
+          onPress={() => router.push("/sign-up")}
+          variant="secondary"
+          fullWidth
+        >
+          Sign Up
+        </Button>
+      </View>
     </View>
   );
 }
